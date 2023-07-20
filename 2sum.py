bf34bfeddcaa27c74d518a933d2ad51a8524f4b5
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-07-20 21:47:33 +0300

    Update 2sum.py
    try yo modify readability

--- 2sum.py
+++ 2sum.py
@@ -2,16 +2,20 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         for i in range(len(nums)):
-            j=i+1
+            j = i+1
             
-            if target==0 or nums[i]!=target:
+            if target==0 or nums[i] != target:
                 for j in range(j,len(nums)):
-                    if nums[j]+nums[i]==target:
-                        x= [i,j]
-                        return x
-                        break 
-                    else: pass
+                    
+                    if nums[j]+nums[i] == target:
+                        answer = [i,j]
+                        return answer
+                        
+                        
+                    else:
+                        pass
             else:
-                y=[i]
-                return y
-                break
+                
+                answer = [i]
+                return answer
+               
